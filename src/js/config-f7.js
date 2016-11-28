0c798857993835353f41f6a5e0c54944c8915a02
Adrian <freefri@freefri.es>
2016-11-28 11:16:48 +0100

    add framework7 config to have panel animations

--- src/js/config-f7.js
+++ src/js/config-f7.js
@@ -1,8 +1,19 @@
 /*jslint browser: true*/
 /*jslint evil: true*/
 /*global console, Framework7*/
+
+MyApp.config = {
+    isIos: (Framework7.prototype.device.ios === true)
+};
+
 MyApp.fw7 = {
   app : new Framework7({
+    material: false,//!MyApp.config.isIos
+    template7Pages: false,
+    swipePanel: 'left',
+    swipePanelActiveArea: '30',
+    swipeBackPage: true,
+    pushState: false,
     animateNavBackIcon: true
   }),
   options : {
