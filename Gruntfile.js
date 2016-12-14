5bc3476743fa2f700fe4ac386fd5d7c960c79075
Adrian <freefri@freefri.es>
2016-12-14 17:14:53 +0100

    add manual configPath to load-grunt-config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,10 @@
 module.exports = function(grunt) {
+    var path = require('path');
+
     require('load-grunt-config')(grunt, {
+        configPath: [
+            path.join(process.cwd(), 'grunt')
+        ],
         data: {
             pkg: grunt.file.readJSON('package.json')
         }
