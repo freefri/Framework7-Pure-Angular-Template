f0a4f411f7e37fbc300fa47b9808129441644f5c
Adrian <freefri@freefri.es>
2016-12-21 16:28:13 +0100

    move main-view template to controller directory

--- src/js/directive/mainView.js
+++ src/js/directive/mainView.js
@@ -2,7 +2,7 @@ MyApp.angular.directive('mainView', function () {
     return {
         restrict: 'E',
         replace: true,
-        templateUrl: 'src/templates/directive/main-view.html',
+        templateUrl: 'src/templates/controller/main-view.html',
         controller: function ($scope) {
         }
     };
