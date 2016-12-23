364cc1248cf5775acd65c6fc050f471658feb508
Adrian <freefri@freefri.es>
2016-12-23 13:08:19 +0100

    add automatic translations to actions

--- src/js/directive/actions.js
+++ src/js/directive/actions.js
@@ -1,10 +1,26 @@
-MyApp.angular.directive('actions', function ($rootScope) {
+MyApp.angular.directive('actions', function ($rootScope, $translate) {
     return {
         restrict: 'A',
         scope: true,
         link: function calendarLink(scope, element, attr) {
             var buttons = scope.$eval(attr.actions);
             var app;
+
+            function translate(btn) {
+                if (btn.text) {
+                    $translate(btn.text).then(function (txt) {
+                        btn.text = txt;
+                    });
+                }
+            }
+
+            buttons.forEach(function (btn) {
+                if (Array.isArray(btn)) {
+                    btn.forEach(translate);
+                } else {
+                    translate(btn);
+                }
+            });
 
             element.on('click', function () {
                 var myCalendar = app.actions(buttons);
