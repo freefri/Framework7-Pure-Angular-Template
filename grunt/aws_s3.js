b37529930b4b60c51fa677abae367c1bbc248fea
Adrian <freefri@freefri.es>
2016-12-29 10:18:25 +0100

    add fonts to s3

--- grunt/aws_s3.js
+++ grunt/aws_s3.js
@@ -8,17 +8,29 @@ module.exports = {
         bucket: '<%= aws.S3.bucketpub %>',
         params: {
             // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
-            ContentEncoding: 'gzip',
             CacheControl: 'max-age=4555000, public'
         }
     },
     prd: {
+        params: {
+            ContentEncoding: 'gzip'
+        },
         files: [
             {
                 expand: true,
                 cwd: 'tmp/s3/',
                 src: ['*'],
                 dest: 'dh/prd/'
+            }
+        ]
+    },
+    fonts: {
+        files: [
+            {
+                expand: true,
+                cwd: '<%= pkg.dist_dir %>/../fonts/',
+                src: ['*'],
+                dest: 'dh/fonts/'
             }
         ]
     }
