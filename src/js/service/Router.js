60e4563664ee38624858df14037e1b85300c22d7
Adrian <freefri@freefri.es>
2017-01-14 11:27:16 +0100

    expose Router methods in IDE

--- src/js/service/Router.js
+++ src/js/service/Router.js
@@ -1,22 +1,22 @@
 MyApp.angular.factory('Router', function ($timeout) {
   'use strict';
 
-  var pub = {};
+  var Router = {};
 
   var fw7 = MyApp.fw7;
 
-  pub.go = function(page) {
+  Router.go = function(page) {
     var mainView = fw7.views[0];
     mainView.router.loadPage(page);
   };
 
-  pub.back = function(opt) {
+  Router.back = function(opt) {
     $timeout(function () {
       var mainView = fw7.views[0];
       mainView.router.back(opt);
     });
   };
 
-  return pub;
+  return Router;
 
 });
