86c24cd762ad1b08c6255eba9ed1d498c3d3fba6
Adrian <freefri@freefri.es>
2017-05-05 11:33:27 +0200

    StateParams.get sends F7App as second parameter of callback function

--- src/js/service/StateParams.js
+++ src/js/service/StateParams.js
@@ -1,9 +1,14 @@
-MyApp.angular.factory('StateParams', function ($rootScope) {
+MyApp.angular.factory('StateParams', function ($rootScope, F7App) {
   'use strict';
 
   var StateParams = {};
   var paramList = {};
   var currentPage;
+
+  var app;
+  F7App.then(function (f7app) {
+    app = f7app;
+  });
 
   var setCurrentpage = function (e, param) {
     currentPage = param.detail.page.name;
@@ -20,7 +25,7 @@ MyApp.angular.factory('StateParams', function ($rootScope) {
   StateParams.get = function (pageName, fn) {
     var getCurrentParams = function (e, param) {
       if (param.detail.page.name == pageName || pageName == '*') {
-        fn(paramList[currentPage]);
+        fn(paramList[currentPage], app);
       }
     };
     $rootScope.$on('f7:pageReinit', getCurrentParams);
